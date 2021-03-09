5d7f2591bd462a37cafeaefb05824e3f34c134a3
flixbeat <flixbeat101@gmail.com>
2021-03-09 20:18:04 +0800

    initial commit

---
+++ Assets/DBConnection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mono.Data.Sqlite;
+using System.Data;
+
+public class DBConnection : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        SQLiteConnection sqlite = new SQLiteConnection();
+        
+        string sql = "SELECT * FROM tbl_items";
+
+        IDataReader reader = sqlite.ExecuteReader(sql);
+
+        while (reader.Read())
+        {
+            int itemId = reader.GetInt32(0);
+            int itemTypeId = reader.GetInt32(1);
+            string itemName = reader.GetString(2);
+            string itemImg = reader.GetString(3);
+            int itemMaxStack = reader.GetInt16(4);
+            int itemAttackUp = reader.GetInt16(5);
+            int itemDefUp = reader.GetInt16(6);
+            int itemHpUp = reader.GetInt16(7);
+            int itemManaUp = reader.GetInt16(8);
+
+            Debug.Log("Item ID: " + itemId +
+                " Item Type ID: " + itemTypeId +
+                " Item Name: " + itemName +
+                " Item Image: " + itemImg +
+                " Item Max Stack: " + itemMaxStack +
+                " Item Attack Up: " + itemAttackUp +
+                " Item Def Up: " + itemDefUp +
+                " Item HP Up: " + itemHpUp +
+                " Item Mana Up: " + itemManaUp);
+
+        }
+
+        sqlite.CloseReader(reader);
+        sqlite.CloseDBConnection();
+    }
+
+    
+}
---
+++ Assets/SQLiteConnection.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+
+public class SQLiteConnection
+{
+    /*
+    sql setup guide: https://answers.unity.com/questions/743400/database-sqlite-setup-for-unity.html
+    
+    1. Create new folder under Assets Folder Rename it Plugins.
+    2. Copy sqlite3.def and sqlite3.dll into Assets/Plugins in your unity project .You can download these files here http://www.sqlite.org/download.html for windows (Precompiled Binaries for Windows)
+    3. Download SQLite Browser http://sourceforge.net/projects/sqlitebrowser/ or http://sqliteadmin.orbmu2k.de/ download SQLite Administrator tool
+    4. Create Database in Assets folder in your unity project using SQLite Browser.
+    5. Copy System.Data.dll and Mono.Data.Sqlite.dll from **C:\Program Files (x86)\Unity \Editor\Data\Mono\lib\mono\2.0* and paste them in your Assets/Plugins* folder in your unity project.
+    6. Add these namespaces using Mono.Data.Sqlite; using System.Data; using System;
+    7. string conn= "URI=file:" + Application.dataPath + "/PickAndPlaceDatabase.s3db";
+        Replace PickAndPlaceDatabase.s3db with your database name
+        void Start () {
+    */
+
+    IDbConnection idbcon; // class for database connection
+    IDbCommand idbcmd; // class for executing sql commands
+    string dbPath;
+
+    public SQLiteConnection() {
+        ConnectToDB();
+    }
+
+    public void ConnectToDB()
+    {
+        dbPath = "URI=file:" + Application.streamingAssetsPath + "/Database/db_game.db";
+        idbcon = (IDbConnection)new SqliteConnection(dbPath);
+        idbcon.Open(); //Open connection to the database.
+        idbcmd = idbcon.CreateCommand();
+    }
+
+    // execute select statements
+    public IDataReader ExecuteReader(string sql)
+    {
+        idbcmd.CommandText = sql;
+        IDataReader reader = idbcmd.ExecuteReader();
+        return reader;
+    }
+
+    public void CloseReader(IDataReader reader)
+    {
+        reader.Close();
+        reader = null;
+        idbcmd.Dispose();
+        idbcmd = null;
+    }
+
+    public void CloseDBConnection()
+    {
+        idbcon.Close();
+        idbcon = null;
+    }
+}
